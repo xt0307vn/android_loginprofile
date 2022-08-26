d5b01bb80888f4b43f73c295b3dca7408b4e3569
xt0307vn <xt0307vn@gmail.com>
2022-08-26 16:16:28 +0700

    day 26/8/22 1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        toolbar = findViewById(R.id.toolbar);
+//        toolbar.setTitle("");
+//        setSupportActionBar(toolbar);
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+//getMenuInflater().inflate(R.menu.main_menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+}
